beaa6a4974e0b9ea55bf972d63f9d2a10fe42ca8
Kommai <dev@kommai.app>
2022-08-05 14:37:00 +0900

    Modified

--- src/Container.php
+++ src/Container.php
@@ -5,23 +5,22 @@ declare(strict_types=1);
 namespace Kommai;
 
 use ArrayAccess;
-use BadMethodCallException;
 use Closure;
 use OutOfBoundsException;
 
 class Container implements ArrayAccess
 {
-    private array $cache = [];
-    private array $definitions;
+    protected array $cache = [];
+    protected array $items = [];
 
-    public function __construct(array $definitions)
+    public function dumpCache(): array
     {
-        $this->definitions = $definitions;
+        return $this->cache;
     }
 
     public function has(string $id): bool
     {
-        return array_key_exists($id, $this->definitions);
+        return array_key_exists($id, $this->items);
     }
 
     public function get(string $id): mixed
@@ -29,13 +28,30 @@ class Container implements ArrayAccess
         if (!$this->has($id)) {
             throw new OutOfBoundsException(sprintf('"%s" does not exist', $id));
         }
-        if (!$this->definitions[$id] instanceof Closure) {
-            return $this->definitions[$id];
+        if (isset($this->cache[$id])) {
+            return $this->cache[$id];
         }
-        if (!isset($this->cache[$id])) {
-            $this->cache[$id] = call_user_func($this->definitions[$id], $this);
+        if ($this->items[$id] instanceof Closure) {
+            return $this->cache[$id] = call_user_func($this->items[$id], $this);
         }
-        return $this->cache[$id];
+        return $this->cache[$id] = $this->items[$id];
+    }
+
+    protected function set(string $id, mixed $item): void
+    {
+        $this->items[$id] = $item;
+    }
+
+    protected function delete(string $id): void
+    {
+        if (!$this->has($id)) {
+            throw new OutOfBoundsException(sprintf('"%s" does not exist', $id));
+        }
+
+        unset($this->items[$id]);
+        if (isset($this->cache[$id])) {
+            unset($this->cache[$id]);
+        }
     }
 
     public function offsetExists(mixed $offset): bool
@@ -45,16 +61,19 @@ class Container implements ArrayAccess
 
     public function offsetGet(mixed $offset): mixed
     {
+
         return $this->get((string) $offset);
     }
 
     public function offsetSet(mixed $offset, mixed $value): void
     {
-        throw new BadMethodCallException('Not allowed to set');
+        // TODO: this allows outsiders to set a value; fix it
+        $this->set((string) $offset, $value);
     }
 
     public function offsetUnset(mixed $offset): void
     {
-        throw new BadMethodCallException('Not allowed to unset');
+        // TODO: this allows outsiders to unset a value; fix it
+        $this->delete((string) $offset);
     }
 }
